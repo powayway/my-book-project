e4fafa4779c5103d1bc55c62f090a0c2e1b35af1
Vincent Descamps <powayway@hotmail.com>
2020-03-09 16:00:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-book-project';
+
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyA1jTU5D9-BQfYUkxuuu6myIZ8Jnscodyo",
+      authDomain: "my-book-app-50e16.firebaseapp.com",
+      databaseURL: "https://my-book-app-50e16.firebaseio.com",
+      projectId: "my-book-app-50e16",
+      storageBucket: "my-book-app-50e16.appspot.com",
+      messagingSenderId: "398243230114",
+      appId: "1:398243230114:web:e8afeaed048ba189ef02fb",
+      measurementId: "G-S6SLM7D7RY"
+    };
+    firebase.initializeApp(config);
+  }
 }
